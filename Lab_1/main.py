263b8796b28a94811f18e77e63f4120b3f98b651
Solodkov Mikhail <55227462+EcLiPsE-code@users.noreply.github.com>
2020-10-16 00:26:45 +0300

    complete lab1, lab2, lab4
    +bug fixed

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -1,4 +1,5 @@
 import sys
+from math import *
 
 
 class NorthwestCornerMethod:
@@ -7,12 +8,14 @@ class NorthwestCornerMethod:
         self.missing_rows = []  # строки, которые пропускаем
         self.missing_columns = []  # столбцы, которые пропускаем
         self.potential = []
-        self.u_i = []
-        self.v_j = []
+        self.u_i = []  # потенциал поставщиков
+        self.v_j = []  # потенциал потребителей
         self.mask = []
-        self.mask_transportation_rates = []
-        self.count_value_point_of_departure: int = 0
-        self.count_value_destination: int = 0
+        self.differences = []
+        self.matrix_for_calculating_potentials = []
+        self.mask_transportation_rates = []  # маска для тарифов перевозок
+        self.count_value_point_of_departure: int = 0  # количество поставщиков
+        self.count_value_destination: int = 0  # количество потребителей
         self.find_min_elements = []  # список всех найденных минимальных тарифов
         self.value_point_of_departure: [int] = []  # количество груза на каждой точке отправления (Запасы)
         self.value_destination: [int] = []  # количество необходимого груза на каждом пункте назначения (Потребности)
@@ -123,16 +126,18 @@ class NorthwestCornerMethod:
         i_temp, j_temp = self._search_min_value_transportation_rates()
         self._calc_value_element_matrix_base_plan(i_temp, j_temp)
 
-    def calc_value_objective_function(self):
+    def calc_value_objective_function(self):  # расчет значения опорного плана
         result = 0
         for i in range(self.count_value_point_of_departure):
             for j in range(self.count_value_destination):
                 if self.matrix_base_plan[i][j] != 0:
                     result += int(self.transportation_rates[i][j]) * int(self.matrix_base_plan[i][j])
+                    "Из {} склада необходимо груз направить в {} магазин ({} единиц)".format(i,j, self.matrix_base_plan[i][j])
 
         return result
 
-    def _generate_mask_for_transportation_rates(self):  # для обозначения строк и столбцов срежи которых ищем минимальное значение
+    def _generate_mask_for_transportation_rates(
+            self):  # для обозначения строк и столбцов среди которых ищем минимальное значение
         self.mask = [[False for i in range(self.count_value_destination)]
                      for j in range(self.count_value_point_of_departure)]
 
@@ -147,8 +152,13 @@ class NorthwestCornerMethod:
         min_column_index = 0
         for i in range(self.count_value_point_of_departure):
             for j in range(self.count_value_destination):
-                if self.mask[i][j] and i == row_index and j == column_index:
-                    if self.transportation_rates[i][j] < temp_min:
+                if i != row_index:
+                    if self.mask[i][j] and j == column_index and self.transportation_rates[i][j] < temp_min:
+                        temp_min = self.transportation_rates[i][j]
+                        min_row_index = i
+                        min_column_index = j
+                elif i == row_index:
+                    if self.mask[i][j] and self.transportation_rates[i][j] < temp_min:
                         temp_min = self.transportation_rates[i][j]
                         min_row_index = i
                         min_column_index = j
@@ -161,42 +171,87 @@ class NorthwestCornerMethod:
             if list_values[i] == (-sys.maxsize):
                 list_values[i] = 0
 
-    def _first_pass(self):
-        row_index = 0
-        column_index = 0
+    @staticmethod
+    def _check(potentials: [int]):
+        for i in range(len(potentials)):
+            if potentials[i] == (-sys.maxsize):
+                return False
+        return True
+
+    def _first_pass(self):  # метод для генерации матрицы для вычисления потенциалов поставщиков и потребителей
+        self.matrix_for_calculating_potentials = [[0 for i in range(self.count_value_destination)]
+                                                  for j in range(self.count_value_point_of_departure)]
+        self.u_i[0] = 0
         for i in range(self.count_value_point_of_departure):
             for j in range(self.count_value_destination):
                 if self.matrix_base_plan[i][j] != 0:
-                    if self.u_i[i] == (-sys.maxsize) and self.v_j[j] == (-sys.maxsize):
-                        row_index, column_index = self._zero_delivery_placement(i, j)
-                        if self.u_i[i] != (-sys.maxsize) and self.v_j[j] == (-sys.maxsize):
-                            self.v_j[j] = self.transportation_rates[row_index][column_index] - self.u_i[i]
-                            self.u_i[i] = self.transportation_rates[row_index][column_index] - self.v_j[j]
-                        elif self.v_j[j] != (-sys.maxsize) and self.u_i[i] == (-sys.maxsize):
-                            self.u_i[i] = self.transportation_rates[row_index][column_index] - self.v_j[j]
-                            self.v_j[j] = self.transportation_rates[row_index][column_index] - self.u_i[i]
-                    elif self.u_i[i] != (-sys.maxsize) and self.v_j[j] == (-sys.maxsize):
+                    if self.u_i[i] != (-sys.maxsize) and self.v_j[j] == (-sys.maxsize):
                         self.v_j[j] = self.transportation_rates[i][j] - self.u_i[i]
                     elif self.v_j[j] != (-sys.maxsize) and self.u_i[i] == (-sys.maxsize):
                         self.u_i[i] = self.transportation_rates[i][j] - self.v_j[j]
 
-    def improvement_transportation_plan(self):  # нахлждение потенциалов для опорного плана
-        row_index = 0
-        column_index = 0
+    def _first_back_pass(self):
+        i_p = 0
+        j_p = 0
+        for i in range(self.count_value_point_of_departure - 1, -1, -1):
+            for j in range(self.count_value_destination - 1, -1, -1):
+                if self.matrix_base_plan[i][j] != 0:
+                    if self.u_i[i] != (-sys.maxsize) and self.v_j[j] == (-sys.maxsize):
+                        self.v_j[j] = self.transportation_rates[i][j] - self.u_i[i]
+                    elif self.v_j[j] != (-sys.maxsize) and self.u_i[i] == (-sys.maxsize):
+                        self.u_i[i] = self.transportation_rates[i][j] - self.v_j[j]
+                    elif self.u_i[i] == (-sys.maxsize) and self.v_j[j] == (-sys.maxsize):
+                        i_p, j_p = self._zero_delivery_placement(i, j)
+                        if self.u_i[i_p] != (-sys.maxsize):
+                            self.v_j[j_p] = self.transportation_rates[i_p][j_p] - self.u_i[i_p]
+                            self.u_i[i_p] = self.transportation_rates[i_p][j_p] - self.v_j[j_p]
+                            if self.u_i[i] != (-sys.maxsize) and self.v_j[j] == (-sys.maxsize):
+                                self.v_j[j] = self.transportation_rates[i][j] - self.u_i[i]
+                            elif self.v_j[j] != (-sys.maxsize) and self.u_i[i] == (-sys.maxsize):
+                                self.u_i[i] = self.transportation_rates[i][j] - self.v_j[j]
+                        else:
+                            self.u_i[i_p] = self.transportation_rates[i_p][j_p] - self.v_j[j_p]
+                            self.v_j[j_p] = self.transportation_rates[i_p][j_p] - self.u_i[i_p]
+                            if self.u_i[i] != (-sys.maxsize) and self.v_j[j] == (-sys.maxsize):
+                                self.v_j[j] = self.transportation_rates[i][j] - self.u_i[i]
+                            elif self.v_j[j] != (-sys.maxsize) and self.u_i[i] == (-sys.maxsize):
+                                self.u_i[i] = self.transportation_rates[i][j] - self.v_j[j]
 
+    def improvement_transportation_plan(self):  # нахлждение потенциалов для опорного плана
         self._generate_mask_for_transportation_rates()
+
         self.potential = [[0 for i in range(self.count_value_destination + 1)]
                           for j in range(self.count_value_point_of_departure + 1)]
 
         self.u_i = [(-sys.maxsize) for i in range(self.count_value_point_of_departure)]
         self.v_j = [(-sys.maxsize) for j in range(self.count_value_destination)]
 
-        self.u_i[0] = 0
         self._first_pass()
-        self._first_pass()
-        self.matrix_base_plan[row_index][column_index] = 0
-        self._replace_with_null(self.u_i)
+        self._first_back_pass()
+
         self._replace_with_null(self.v_j)
+        self._replace_with_null(self.u_i)
+
+    def calc_difference_free_cells(self):
+        self.differences = [[sys.maxsize for i in range(self.count_value_destination)]
+                            for j in range(self.count_value_point_of_departure)]
+        for i in range(self.count_value_point_of_departure):
+            for j in range(self.count_value_destination):
+                if self.matrix_base_plan[i][j] == 0:
+                    self.differences[i][j] = self.transportation_rates[i][j] - (self.u_i[i] + self.v_j[j])
+
+    def check_optimality(self):
+        for i in range(self.count_value_point_of_departure):
+            for j in range(self.count_value_destination):
+                if self.differences[i][j] < 0:
+                    return False
+        return True
+
+    def print_res(self):
+        for i in range(self.count_value_point_of_departure):
+            for j in range(self.count_value_destination):
+                if self.matrix_base_plan[i][j] != 0:
+                    print('Из {} склада необходимо груз направить в {} магазин ({} единиц)'.format(i + 1, j + 1, self.matrix_base_plan[i][j]))
 
 
 if __name__ == '__main__':
@@ -216,3 +271,12 @@ if __name__ == '__main__':
     method.improvement_transportation_plan()
     print(method.u_i)
     print(method.v_j)
+    print("Определенные разности для свободных клеток: ")
+    method.calc_difference_free_cells()
+    print(method.differences)
+    if method.check_optimality():
+        print("Данный план является оптимальным, тк все оценки свободных клеток удовлетворяют условию"
+              "u_i + v_j <= c_ij")
+        method.print_res()
+    else:
+        print("Данный план не оптимальный производим улучшение плана")
--- Lab_2/main.py
+++ Lab_2/main.py
@@ -2,50 +2,167 @@ import sys
 
 class HungarianMethod:
     def __init__(self) -> None:
-        self.matrix_base_plan = []  # первоначальный опорный план
-        self.transportation_rates = None  # матрица таррифов
-        self.mechanisms = []  # механизмы, которые используются для выполнения работы
-        self.objects = []  # объекты, работу над которыми необходимо выполнить
-        self.count_mechanisms: int = None  # количество механизмов
-        self.count_objects: int = None # количество объектов, работу над которыми необходимо выполнить
+        self._matrix = None
+        self.col = None
+        self.row = None
+        self._mask = []  #True - (0 помечен точкой), (-1) - (0 вычеркнут)
+        self.result = None
+        self._min_elements_in_lines: [int] = None  # минимальные элементы в каждой строке
+        self._min_elements_in_columns: [int] = None  # минимальные элементы в каждодм столбце
 
-    def set_count_mechanisms(self, count) -> None:
-        self.count_mechanisms = count
+    def set_row(self, row):
+        self.row = row
 
-    def set_count_objects(self, count) -> None:
-        self.count_objects = count
+    def set_col(self, col):
+        self.col = col
 
-    def set_transportation_rates(self, transportation_rates: [int]) -> None:
-        self.transportation_rates = transportation_rates
+    def get_row(self):
+        return self.row
 
-    def get_count_mechanisms(self) -> int:
-        return self.count_mechanisms
+    def get_col(self):
+        return self.col
 
-    def get_count_objects(self) -> int:
-        return self.count_objects
+    def set_matrix(self, matrix) -> None:
+        self._matrix = matrix
 
-    def get_transportation_rates(self) -> [int]:
-        return self.transportation_rates
+    def get_matrix(self) -> None:
+        for i in range(len(self._matrix)):
+            for j in range(len(self._matrix[0])):
+                print("{}".format(self._matrix[i][j]), end="\t")
+            print()
 
-    def line_casting(self): # приведение по строкам
-        lines_casting = []
-        for i in range(self.get_count_mechanisms()):
-            temp_min = -sys.maxsize
-            for j in range(self.get_count_objects()):
-                if self.transportation_rates[i][j] < temp_min:
-                    temp_min = self.transportation_rates[i][j]
-            lines_casting.append(temp_min)
+    def _search_min_elements_in_lines(self) -> None:  # функция для поиска минимльного значения в каждой строке
+        self._min_elements_in_lines = [0 for i in range(self.row)]
+        for i in range(self.row):
+            temp_min = sys.maxsize
+            for j in range(self.col):
+                if self._matrix[i][j] < temp_min:
+                    temp_min = self._matrix[i][j]
+            self._min_elements_in_lines[i] = temp_min
 
-    def column_cast(self): # приведение по столбцам
-        pass
+    def _search_min_elements_in_columns(self) -> None:  # поиск мин значений в каждом столбце
+        self._min_elements_in_columns = [0 for i in range(self.col)]
+        for i in range(self.col):
+            temp_min = sys.maxsize
+            for j in range(self.row):
+                if self._matrix[j][i] < temp_min:
+                    temp_min = self._matrix[j][i]
+            self._min_elements_in_columns[i] = temp_min
+
+    def _calc_min_elements_in_lines(self) -> None:  # отнимает найденное минимальное значение от всех эл строки
+        for i in range(self.row):
+            for j in range(self.col):
+                self._matrix[i][j] -= self._min_elements_in_lines[i]
+
+    def _calc_min_elements_in_columns(self) -> None:  # отнимает найденное минимальное значение от всех эл столбца
+        for i in range(self.col):
+            for j in range(self.row):
+                self._matrix[j][i] -= self._min_elements_in_columns[i]
+
+    def getting_zeros(self) -> None:  # получение 0 в строках и столбцах
+        self._search_min_elements_in_lines()
+        self._calc_min_elements_in_lines()
+        self._search_min_elements_in_columns()
+        self._calc_min_elements_in_columns()
+
+    def find_optimal_solution(self) -> None:  # нахождение первноначального плана
+        self._mask = [[False for i in range(self.col)]
+                     for j in range(self.row)]
+
+        for i in range(self.row):   #True - помечен точкой, -1 - зачеркнут
+            for j in range(self.col):
+                if self._matrix[i][j] == 0 and self._mask[i][j] != True and self._mask[i][j] != -1:
+                    self._mask[i][j] = True
+                    for k in range(self.col):  # зачеркиваем все остальные нули в строке
+                        if self._matrix[i][k] == 0 and self._mask[i][k] == False:
+                            self._mask[i][k] = -1
+                    for m in range(self.row):
+                        if self._matrix[m][i] == 0 and self._mask[i][m] == False:
+                            self._mask[m][i] = -1
+
+    def check_result(self) -> bool:  # проверка количества полученных нулей в mask (n = column)
+        self.hungarian()
+        if len(self.result) == self.col:
+            print("Тк число отмеченных 0 равно n, то решение является полным, а план оптимальным, "
+                  "производим расчет целевой функции...")
+            print("Значение целевой функции равно: {}".format(self.calc_objective_function()))
+        else:
+            print("План не оптимальный выполняем перерасчет...")
+            self.hungarian()
+            self.check_result()
+
+    def hungarian(self):
+        # Значения, вычитаемые из строк (u) и столбцов (v)
+        u = [0 for i in range(self.row)]
+        v = [0 for j in range(self.col)]
+
+        # Индекс помеченной клетки в каждом столбце
+        mark_indexes = [-1 for i in range(self.col)]
+
+        count = 0
+        for i in range(self.row):
+            links = [-1 for i in range(self.col)]
+            mins = [sys.maxsize for i in range(self.col)]
+            visited = [0 for i in range(self.col)]
+
+            # Разрешение коллизий (создание "чередующейся цепочки" из нулевых элементов)
+            markedI = i
+            markedJ = -1
+            j = 0
+            while markedI != -1:
+                j = -1
+                for j_1 in range(self.col):
+                    if visited[j_1] != 1:
+                        if self._matrix[markedI][j_1] - u[markedI] - v[j_1] < mins[j_1]:
+                            mins[j_1] = self._matrix[markedI][j_1] - u[markedI] - v[j_1]
+                            links[j_1] = markedJ
+                        if j == -1 or (mins[j_1] < mins[j]):
+                            j = j_1
+                delta = mins[j]
+                for j_1 in range(self.col):
+                    if visited[j_1] == 1:
+                        u[mark_indexes[j_1]] += delta
+                        v[j_1] -= delta
+                    else:
+                        mins[j_1] -= delta
+                u[i] += delta
+
+                visited[j] = 1
+                markedJ = j
+                markedI = mark_indexes[j]
+                count += 1
+
+            while links[j] != -1:
+                mark_indexes[j] = mark_indexes[links[j]]
+                j = links[j]
+            mark_indexes[j] = i
+
+        result = []
+        for j in range(self.col):
+            if mark_indexes[j] != -1:
+                result.append([mark_indexes[j], j])
+        self.result = result
+
+    def calc_objective_function(self) -> int:
+        sum = 0
+        for k in range(len(self.result)):
+            i = self.result[k][0]
+            j = self.result[k][1]
+            sum += self._matrix[i][j]
+        return sum
 
 if __name__ == "__main__":
     method = HungarianMethod()
-    print("Количество механизмов, которые используются для выполнения работы на объектах")
-    method.set_count_mechanisms(int(input()))
-    print("Количество объектов, над которыми необходимо выполнить работу")
-    method.set_count_objects(int(input()))
-    print("Введите матрицу тарифов")
-    method.set_transportation_rates([list(map(int, input().split()))
-                                     for i in range(method.get_count_mechanisms())])
-    print(method.get_transportation_rates())
+    print("Введите количество вакансий(столбцы): ")
+    method.set_col(int(input()))
+    print("Введите количество кандидатов(строки): ")
+    method.set_row(int(input()))
+    print("Введите матрицу: ")
+    method.set_matrix([list(map(int, input().split()))
+                                     for i in range(method.get_row())])
+    method.check_result()
+    print("Матрица: ")
+    method.get_matrix()
+    print("Матрица после приведения по строкам и столбцам")
+    method.getting_zeros()
+    method.get_matrix()
